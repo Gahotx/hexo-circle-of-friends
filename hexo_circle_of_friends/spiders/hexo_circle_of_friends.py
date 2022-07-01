fe4a5018a76397f51af1c24a0c53ccda8a8dfd3f
Gahotx <53534938+Gahotx@users.noreply.github.com>
2022-07-01 11:37:03 +0800

    Update hexo_circle_of_friends.py

--- hexo_circle_of_friends/spiders/hexo_circle_of_friends.py
+++ hexo_circle_of_friends/spiders/hexo_circle_of_friends.py
@@ -41,7 +41,7 @@ post_parsers = [
 ]
 # 默认feed后缀
 feed_suffix = [
-    "atom.xml", "feed/atom", "rss.xml", "rss2.xml", "feed", "index.xml"
+    "atom.xml", "rss.xml", "index.xml", "feed", "index.php/feed", "feed/atom", "rss2.xml"
 ]
 
 
